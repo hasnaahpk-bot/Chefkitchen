f309226a342fca698810c6398e6d1036c7c9e7b6
dilshad <hmydilshadkt@gmail.com>
2025-12-19 15:18:45 +0530

    feat:changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import Landingpg from './components/Landingpg'
-import Home from './components/Home'
-import { Route, Routes } from 'react-router-dom'
-import Offer from './components/Offer'
-import Wishlist from './components/Wishlist'
-import  Mail  from './components/Mail'
-import Notify from './components/Notify'
+import React from "react";
+import Landingpg from "./components/Landingpg";
+import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
+import Offer from "./components/Offer";
+import Wishlist from "./components/Wishlist";
+import Mail from "./components/Mail";
+import Notify from "./components/Notify";
+
+// TODO : Create a sperate folder under the src named "CONSTANTS" and move all the constants to that folder
+// TODO : Remove unused imports and variable that will causes the performance issue
+// TODO : Split this component into smaller components and move them , To make the code more clean
+// TODO : Need to sepreate the logic
+// TODO : Make the Empty dish text ["No dishes match your selection"] to the ceneter of the screen
+// TODO : Use context here for the state management
+
+// TODO : Create a seperate folder for pages and move the pages from the components
+// TODO : Make this entire application responsive [ The list layout won't break in each screen ]
 
 const App = () => {
   return (
@@ -17,10 +27,9 @@ const App = () => {
         <Route path="/like" element={<Wishlist />} />
         <Route path="/mail" element={<Mail />} />
         <Route path="/notify" element={<Notify />} />
-
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
